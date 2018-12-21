bc6b9730058b060e6a4401b411270d8b181362ca
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2018-12-20 20:03:41 -0500

    Update

--- Email/Email.java
+++ Email/Email.java
@@ -1,39 +1,47 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.ArrayList;
 
 public class Email {
+
+  private ArrayList<Email> emailList = new ArrayList<Email>();
 
   private String firstName;
   private String lastName;
   private String password;
   private String department;
-  private int mailboxCapacity;
+  private String email;
+  private int defaultPasswordLength = 10;
+  private int mailboxCapacity = 500;
   private String alternateEmail;
+  private String companySuffix = "company.com";
 
   public Email(){
+
     this.firstName = setFirstName();
     this.lastName = setLastName();
     this.department = setDepartment();
-    this.password = setPassword();
-
+    this.password = setPassword(defaultPasswordLength);
+    email = firstName.toLowerCase() + "." + lastName.toLowerCase() +"@" +
+            department + "." + companySuffix;
+    System.out.println("Your email is: " + email);
+    System.out.println("and  Password: " + this.password);
+    
   }
 
   private String setFirstName(){
     System.out.println("Please Enter your First Name: ");
-    try(Scanner kb = new Scanner(System.in)){
-      String firstName = kb.next();
-    }catch(InputMismatchException ex){
-      System.out.println("Please enter the name");
-    }
+    Scanner kb = new Scanner(System.in);
+    String firstName = kb.next();
+
     return firstName;
   }
+
   private String setLastName(){
     System.out.println("Please Enter your LastName: ");
-    try(Scanner kb = new Scanner(System.in)){
-      String lastName = kb.next();
-    }catch(InputMismatchException ex){
-      System.out.println("Please enter the name");
-    }
+    Scanner kb = new Scanner(System.in);
+    String lastName = kb.next();
+
     return lastName;
   }
 
@@ -53,9 +61,38 @@ public class Email {
 
   }
 
-  private String setPassword(){
-    
+  private String setPassword(int length){
+    String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%";
+    char[] password = new char[length];
+    for (int i = 0; i < length; i++){
+      int rand = (int) (Math.random() * passwordSet.length());
+      password[i] = passwordSet.charAt(rand);
+    }
+    return new String(password);
   }
 
+  public void setMailboxCapacity(int capacity){
+    this.mailboxCapacity = capacity;
+  }
+
+  public void setAlternateEmail(String altEmail){
+    this.alternateEmail = altEmail;
+  }
+
+  public void setPassword(String password){
+    this.password = password;
+  }
+
+  public int getMailboxCapacity(){
+    return mailboxCapacity;
+  }
+
+  public String getAlternateEmail(){
+      return alternateEmail;
+  }
+
+  public String getPassword(){
+    return password;
+  }
 
 }
--- Email/EmailApp.java
+++ Email/EmailApp.java
@@ -1,5 +1,7 @@
 public class EmailApp {
   public static void main (String...args){
-    
+    //varification();
+    Email email1 = new Email();
   }
+
 }
