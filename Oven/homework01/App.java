70b036c39ef4dec0189b5f8e916cb52088fcb5c3
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-02-01 23:33:41 -0500

    corrections and additions

--- Oven/homework01/App.java
+++ Oven/homework01/App.java
@@ -1,10 +1,31 @@
 public class App{
   public static void main (String []args){
-    Contact c1 = new Contact("Faisal", "Ibrahim", "917-459-9398");
-    System.out.println(c1);
-    Contact c2 = new Contact("faisal", "Ibrahim", "917-459-9398");
-    System.out.println(c2);
-    System.out.println();
-    System.out.println("YES or NO " + c1.compareTo(c2));
+
+    // create a contact using default constructor with no data fields
+    Contact contact1 = new Contact();
+    System.out.println("This the the object with no data fields\n" + contact1);
+
+    // create 2 contacts with the same data fiels
+    Contact contact2 = new Contact("John", "Smith", "917-459-9398");
+    Contact contact3 = new Contact("John", "Smith", "917-459-9398");
+    Contact contact4 = new Contact("Harry", "Potter", "718-123-4321");
+    Contact contact5 = new Contact("Nancy", "Smith", "917-459-9398");
+    // comparing the two contacts if they are equals
+    printWithPriority(contact2, contact5);
+    System.out.println(contact2.compareTo(contact5));
+
   }
+
+  public static void printWithPriority(Contact c1, Contact c2){
+    if (c1.compareTo(c2) > 0){
+      System.out.println(c1.getFirstName() + "\n comes before " + c2.getFirstName());
+    }
+    if (c1.compareTo(c2) < 0){
+      System.out.println(c2 + "\n comes before " + c1);
+    }
+    else {
+      System.out.println(c2 + "\n and " + c1 + "\n are equal");
+    }
+  }
+
 }
