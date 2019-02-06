44d2986667838a26c4155fecc1a4dce5a275328f
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-02-06 18:56:57 -0500

    Homework folder created

--- Oven/ClassWork/Compare.java
+++
@@ -1,9 +0,0 @@
-public class Compare{
-  public int compareTo(Contact another){
-    int lastComp = lastName.comapreTo(another.lastName);
-      if(lastComp == 0){
-        return firstName.compareTo(another.firstName);
-      }
-      else return lastComp;
-  }
-}
