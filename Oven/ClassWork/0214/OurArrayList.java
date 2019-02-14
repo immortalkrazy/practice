8f436d8ba032d7eda619c64142b90b8d363f9cae
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-02-14 10:49:53 -0500

    class 0214

---
+++ Oven/ClassWork/0214/OurArrayList.java
@@ -0,0 +1,170 @@
+import java.util.*;
+
+public class OurArrayList <E> implements Iterable<E> {
+
+  private class OurArrayIterator implements Iterator<E> {
+    // private class variable
+    private int index;
+
+    public OurArrayIterator() {
+      index = 0;
+    }
+
+    public boolean hasNext() {
+      return index < numElements;
+    }
+
+    public E next() {
+      index ++;
+      return get(index - 1);
+    }
+
+    public void remove(){
+      throw new UnsupportedOperationException();
+      //remove(index - 1);
+    }
+  }// end of class
+
+
+  private E[] arr;
+  private int numElements;
+
+  public Iterator<E> iterator() {
+    return new OurArrayIterator();
+  }
+
+  public OurArrayList(){
+      numElements  = 0;
+      arr = (E[])new Object[20];
+  }
+  // add an element to the array
+  public boolean add (E e){
+    // check if the size is avaialable
+    if (numElements == arr.length) {
+      grow();
+    }
+    //add the element to array
+    arr[numElements] = e;
+    numElements++;
+
+    return true;
+  }
+
+  private void grow(){
+    // create a new array of double the length
+    E[] temp = (E[]) new Object[2 * arr.length];
+    // now copy the old array into the new array
+    for (int i = 0; i < arr.length; i++){
+      temp[i] = arr[i];
+    }
+    // now rename temp array to the actual arraylist
+    arr = temp;
+  }
+  // get the size of the current array
+  public int size(){
+      return numElements;
+  }
+  // check if array is empty
+  public boolean empty(){
+    return numElements == 0;
+  }
+
+  public E remove(int index) {
+    E value = arr[index];
+    for(int i = index+1; i < numElements; i++){
+      arr[i - 1] = arr[i];
+    }
+    numElements--;
+    return value;
+  }
+
+  public boolean remove(Object obj){
+    // use indexOf method
+    int index = indexOf(obj);
+    if (index == -1){
+      return false;
+    }
+    // use other remove method
+    remove(index);
+
+    return true;
+  }
+
+  // get the element in the array from a specific index
+  public E get(int index){
+    if(index >= numElements || index < 0){
+      throw new IndexOutOfBoundsException();
+    }
+    return arr[index];
+  }
+  // set a certain value at a certain index
+  public E set(int index, E value){
+    if (index < 0 || index >= numElements){
+      throw new IndexOutOfBoundsException();
+    }
+    E old = arr[index];
+    arr[index] = value;
+
+    return old;
+  }
+  //
+  public void clear(){
+    numElements = 0;
+  }
+  // check if the elemnts exists
+  public boolean contains(Object obj){
+  //use inedxOf method in our own implentation
+    return indexOf(obj)!= -1;
+  }
+  //
+  // public boolean equals(Object obj);
+  //
+  public int indexOf(Object obj){
+
+    for (int i = 0; i < numElements; i++){
+      if(arr[i].equals(obj)){
+        return i;
+      }
+    }
+    return -1;
+  }
+
+
+  // set with the new array
+  public void add(int index, E value){
+    // check if there is enough room
+    if (numElements == arr.length){
+      grow();
+    }
+    // move backwards and add elements
+    /* 2 3 4 5 6
+       2 3 1 4 5 6 */
+    for (int i = numElements-1; i >= index; i--){
+      arr[i+1] = arr[i];
+    }
+    // put the element at the index
+    arr[index] = value;
+    numElements++;
+  }
+
+  public String toString() {
+    String result = "";
+
+    for(E e: this){
+      result += e + " ";
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+
+    OurArrayList<Integer> list;
+    list = new OurArrayList<>();
+
+    for (int i = 0; i < 10; i++){
+      list.add(i);
+    }
+
+  }
+
+}// end of class
