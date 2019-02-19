699751405542e9613b1e18e45a8bc3db184c6ef0
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-02-19 10:43:46 -0500

    classwork

---
+++ Oven/ClassWork/0219/BinarySearch.java
@@ -0,0 +1,14 @@
+public class BinarySearch {
+
+
+
+  public int binarySearch(int[] arr, int target){
+    int n = arr.length;
+    int start = 0;
+    int end = n-1;
+
+    while (start <= end){
+      int mid = (start)
+    }
+  }
+}
