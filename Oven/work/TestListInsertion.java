0d838a995235327bf9284a66a6a0c7d15ede6cd4
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-02-21 22:16:09 -0500

    test insertions

--- Oven/work/TestListInsertion.java
+++ Oven/work/TestListInsertion.java
@@ -15,8 +15,34 @@ public class TestListInsertion {
     ArrayList<Integer> arrayList = new ArrayList<Integer>();
     LinkedList<Integer> linkedList = new LinkedList<Integer>();
 
+    app.makeRandomList(arrayList, initSize, max);
+    app.makeRandomList(linkedList, initSize, max);
 
+    app.testInsertion(arrayList, maxInsertions);
+    app.testInsertion(linkedList, maxInsertions);
   }
 
+  // make a random list
+  private List<Integer> makeRandomList (List<Integer> list, int size, int max) {
+    Random rng = new Random();
+    for (int i = 0; i < size; i++) {
+      list.add(rng.nextInt(max));
+    }
+    return list;
+  }
+
+  private <T> void testInsertion(List<T> list, int maxInsertions) {
+    Random rng = new Random();
+    T t = list.get(0);
+
+    long startTime = System.nanoTime();
+    for (int i = 0; i < maxInsertions; i++) {
+      list.add(0, t);
+    }
+    long endTime = System.nanoTime();
+
+    System.out.println("Time to inserting randomly " + maxInsertions +
+      " elments to a " + list.getClass().getName() + ": " + (endTime - startTime));
+  }
 
 }
