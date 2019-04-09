5465591648269aafae627f5568d8639310671810
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-04-09 13:00:01 -0400

    classwork 0409

---
+++ Oven/ClassWork/0409/Occurrences.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Occurrences {
+
+    public staic void main(String[] args) {
+
+        Scanner s = new Scanner(new )
+    }
+
+
+
+
+
+    private static <K, V> void printMap (Map<K,V> map) {
+
+        for (K key: map.keySet()) {
+            V value = map.get(key);
+            System.out.println(key + " : " + value);
+        }
+
+        /*
+        for (Map.Entry<K, V> entry: map.entrySet()) {
+
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+
+        }
+        */
+    }
+
+    Map<Integer, List<Character>> sorted;
+    sorted = new TreeMap<>();
+
+    invertPap(occ, sorted);
+
+
+
+
+}
