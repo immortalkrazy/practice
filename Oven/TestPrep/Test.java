9b81660a709c3943f2c3c30e4079b53a62477cc9
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-05-12 19:11:58 -0400

    hi

--- Oven/TestPrep/Test.java
+++ Oven/TestPrep/Test.java
@@ -1,9 +1,44 @@
 public class Test {
 
+    public String str;
+
     public static void main(String[] args) {
 
         String s1 = "1";
         String s2 = "9";
         System.out.println(s1.compareTo(s2));
+
+        System.out.println(75 % 15);
+
+        String str = "defs";
+
+
+        System.out.println(getIndex(str));
+        System.out.println(index(str));
+
     }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((str == null) ? 0 : str.hashCode());
+        return result;
+    }
+
+
+    private static int getIndex(String key) {
+
+        return key.hashCode() % 15;
+
+    }
+    private static int index (String str) {
+
+        int total = 0;
+        for (int i = 0; i < str.length(); i++) {
+            total += (str.charAt(i) - 'a');
+        }
+        return total % 15;
+    }
+
+
 }
