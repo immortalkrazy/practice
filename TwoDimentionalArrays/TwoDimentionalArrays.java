03013ab9591f603eeada5982bfc996e25e140471
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-06-27 18:44:19 -0400

    two dimensional array

---
+++ TwoDimentionalArrays/TwoDimentionalArrays.java
@@ -0,0 +1,18 @@
+public class TwoDimentionalArray {
+
+    public static void main (String []args) {
+
+
+    }
+    public static void createRaggedArray() {
+        try(Scanner sc = new Scanner(System.in)) {
+
+            int[][] tiangleArray;
+            traingleArray = new int[asked][];
+
+        }catch {
+                (System.out.println("Error"));
+        }
+
+    }
+}
